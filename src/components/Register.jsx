5168351edce7a778993e9051252e1251f8e8bdf0
samim <aktarsamim529@gmail.com>
2023-12-02 11:47:11 +0530

    Intial Commit

---
+++ src/components/Register.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Register() {
+  return (
+    <div>
+      Register
+    </div>
+  )
+}
+function RegisterWithMassage() {
+    return (
+      <div>
+        Register with new user.
+      </div>
+    )
+  }
+  function RegisterWithEmail() {
+    return (
+      <div>
+        Register with new user.
+      </div>
+    )
+  }
+  export {RegisterWithMassage,RegisterWithEmail}
+
+export default Register
