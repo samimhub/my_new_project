5cb6a4bcd12c62991c2a50727667839fd1136700
samim <aktarsamim529@gmail.com>
2023-12-11 22:39:38 +0530

    Secoend half commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+ const Profile=()=>{
+    return(
+      <>
+      <h1>A photo of smart boy</h1>
+      <img src="boy.jpg" alt="smart_boy"/>
+      <ul>
+        <li>Samim Aktar</li>
+        <li>Akram Mondal</li>
+        <li>Tarif Aktar</li>
+      </ul>
+      </>
+    );
+  }
+  export default Profile;
