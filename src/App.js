563b231e0bcb293c20ad56f280bd41e98c908cb8
samim <aktarsamim529@gmail.com>
2023-12-11 22:52:49 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import './App.css';
 import Contain from './components/Contain';
 import {Login} from './components/Login';
 import {Register} from './components/Register';
+import Footer from './components/Footer';
 
 const App=() =>{
   return (
-    <div className='flex text-center justify-center'>
+    <div>
+    {/*<div className='flex text-center justify-center'>
       <Routes>
       <Route path ='/login' element = {<Login/>}/>
       <Route path ='Login/register' element = {<Register/>}/>
-      </Routes>
-  {/*<Contain/>*/}
+  </Routes>*/}
+  <Footer/>
 
     
     </div>
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+      <button>click it</button>
+    </div>
+  )
+}
+
+export default Footer;
